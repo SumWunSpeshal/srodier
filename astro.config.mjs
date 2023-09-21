df3706c234eb06b7e883876d23164f1df25414f6
Sacha Rodier <mail@srodier.com>
2023-09-21 16:07:22 +0200

    use singleQuotes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 // Use Vercel Edge Functions (Recommended)
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless';
 // Can also use Serverless Functions
 // import vercel from '@astrojs/vercel/serverless';
 // Or a completely static build
 // import vercel from '@astrojs/vercel/static';
 
 export default defineConfig({
-  output: "server",
+  output: 'server',
   adapter: vercel({
     edgeMiddleware: true,
   }),
