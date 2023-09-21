a877720455bbed0082f7c5b5ba68f60cf8c0b912
Sacha Rodier <mail@srodier.com>
2023-09-21 15:52:02 +0200

    update deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 // Use Vercel Edge Functions (Recommended)
-import vercel from '@astrojs/vercel/edge';
+import vercel from "@astrojs/vercel/serverless";
 // Can also use Serverless Functions
 // import vercel from '@astrojs/vercel/serverless';
 // Or a completely static build
 // import vercel from '@astrojs/vercel/static';
 
 export default defineConfig({
-  output: 'server',
-  experimental: {
-    assets: true
-   },
+  output: "server",
   adapter: vercel({
-    imageService: true,
+    edgeMiddleware: true,
   }),
 });
