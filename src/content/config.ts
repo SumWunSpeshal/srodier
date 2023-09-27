ccab1233657a06fce6a7a8290779276d9c86d23a
Sacha Rodier <mail@srodier.com>
2023-09-27 22:50:44 +0200

    add Sidebar

--- src/content/config.ts
+++ src/content/config.ts
@@ -26,10 +26,24 @@ const indexCollection = defineCollection({
   }),
 });
 
+const sidebarCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    name: z.string(),
+    title: z.string(),
+    description: z.string(),
+    contact: z.object({
+      city: z.string(),
+      email: z.string().email(),
+    }),
+  }),
+});
+
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
   index: indexCollection,
   skills: skillCollection,
   workExperience: workExperienceCollection,
+  sidebar: sidebarCollection,
 };
