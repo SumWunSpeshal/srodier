56359f70a86193bd1fdcfb46461ae70240f27d05
Sacha Rodier <mail@srodier.com>
2023-10-30 14:56:45 +0100

    implement contact form

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,6 +3,10 @@
 
 interface ImportMetaEnv {
   readonly PUBLIC_VERCEL_ANALYTICS_ID: string;
+  readonly MAIL_HOST: string;
+  readonly MAIL_USER: string;
+  readonly MAIL_PASSWORD: string;
+  readonly MAIL_TO: string;
 }
 
 interface ImportMeta {
--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,13 +1,46 @@
 import type { APIRoute } from 'astro';
+import { createTransport } from 'nodemailer';
+
+const transporter = createTransport({
+  host: import.meta.env.MAIL_HOST,
+  port: 587,
+  secure: false,
+  auth: {
+    user: import.meta.env.MAIL_USER,
+    pass: import.meta.env.MAIL_PASSWORD,
+  },
+});
 
 export const POST: APIRoute = async ({ request }) => {
-  const asdasd = await request.formData();
-  console.log(asdasd);
+  const formData = await request.formData();
 
-  return new Response(
-    JSON.stringify({
-      message: 'Success!',
-    }),
-    { status: 200 },
-  );
+  try {
+    await transporter.sendMail({
+      from: `"srodier contact form" <${import.meta.env.MAIL_USER}>`,
+      to: import.meta.env.MAIL_TO,
+      subject: 'Contact request',
+      text: 'Success! Email successfully sent',
+      html: `
+        <div>Name: ${formData.get('name')}</div>
+        <div>Email: ${formData.get('email')}</div>
+        <div>Message: ${formData.get('message')}</div>
+      `,
+    });
+
+    return new Response(
+      JSON.stringify({
+        message: 'Success!',
+      }),
+      { status: 200 },
+    );
+  } catch (error) {
+    console.error(error);
+
+    return new Response(
+      JSON.stringify({
+        message: error,
+      }),
+      { status: 200 },
+    );
+  }
 };
